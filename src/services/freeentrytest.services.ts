8687012b5f7a747b7502ce49f10f78d756b71eab
SmithSonNguyen <nguyenducthanh14112003@gmail.com>
2025-10-13 14:45:23 +0700

    fix_sort_data_free_entry_test

--- src/services/freeentrytest.services.ts
+++ src/services/freeentrytest.services.ts
@@ -1,10 +1,31 @@
 import FreeEntryTest from '~/models/schemas/FreeEntryTest.schema'
 
 class FreeEntryTestService {
+  async sortData() {
+    // Lấy toàn bộ document, sắp xếp theo part và sắp xếp từng mảng questions theo number
+    const tests = await FreeEntryTest.aggregate([
+      {
+        $addFields: {
+          questions: {
+            $sortArray: {
+              input: '$questions',
+              sortBy: { number: 1 }
+            }
+          }
+        }
+      },
+      {
+        $sort: {
+          part: 1,
+          'questions.0.number': 1 // Sắp xếp theo số câu hỏi đầu tiên
+        }
+      }
+    ])
+    return tests
+  }
   async getFullTest() {
     try {
-      // lấy toàn bộ document trong collection
-      const tests = await FreeEntryTest.find()
+      const tests = await this.sortData()
       return tests
     } catch (error) {
       if (error instanceof Error) {
